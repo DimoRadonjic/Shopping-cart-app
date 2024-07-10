187befbf4cc4bc82015469c101d9ee1cd8831830
Dimitrije_Radonjic <dimitrije.radonjic@acme.co.me>
2024-07-10 21:39:26 +0200

    update package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import {
   ProductComponent,
   ProductListComponent,
@@ -22,29 +22,22 @@ import {
 import { FormsModule } from '@angular/forms';
 import { ProductListService } from './services';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    ProductComponent,
-    ProductListComponent,
-    ProductModalComponent,
-    ProductInfoComponent,
-    RatingComponent,
-    CartComponent,
-    CartModalComponent,
-    ListOptionsComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    NgbModule,
-    NgbPaginationModule,
-    NgbModalModule,
-    NgbRatingModule,
-  ],
-  providers: [ProductListService],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [
+        AppComponent,
+        ProductComponent,
+        ProductListComponent,
+        ProductModalComponent,
+        ProductInfoComponent,
+        RatingComponent,
+        CartComponent,
+        CartModalComponent,
+        ListOptionsComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        NgbModule,
+        NgbPaginationModule,
+        NgbModalModule,
+        NgbRatingModule], providers: [ProductListService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
